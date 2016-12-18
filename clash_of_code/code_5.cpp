c22540d3a72e0af9033b71b23b578d361b01c302
Marie Chantal T. Tan <mariechantaltan@gmail.com>
2016-12-17 21:02:12 -0800

    Create code_5.cpp

---
+++ clash_of_code/code_5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string line;
+    cin >> line;
+    cout << line.at(line.size() - 1) << endl;
+}
