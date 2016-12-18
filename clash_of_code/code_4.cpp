024af5223ef2f6d70273a0a19e0a9c17bc1e35b2
Marie Chantal T. Tan <mariechantaltan@gmail.com>
2016-12-17 20:48:03 -0800

    Create code_4.cpp

---
+++ clash_of_code/code_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;  
+    cout << n * n * 100 << endl;
+}
